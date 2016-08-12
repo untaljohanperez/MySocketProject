d4feff05445723fd31bfd82cdc9b4a5237c8a075
Kedwin Johan Pérez Zea - Ceiba Software <kedwin.perez@ceiba.com.co>
2016-08-12 11:17:43 -0500

    Counting down in all clients

--- Scripts/Controllers/ProcessManagerController.js
+++ Scripts/Controllers/ProcessManagerController.js
@@ -4,7 +4,7 @@ app.factory('mySocket', function (socketFactory) {
   return socketFactory();
 });
 
-app.controller('ProcessManagerController', ['$scope', 'mySocket', function($scope, mySocket){
+app.controller('ProcessManagerController', ['$scope', '$timeout', '$interval', 'mySocket', function($scope, $timeout, $interval, mySocket){
 	$scope.processList = [];
 	$scope.newProcess = { process: null, processingTime: null};
 
@@ -12,7 +12,16 @@ app.controller('ProcessManagerController', ['$scope', 'mySocket', function($scop
 		if($scope.newProcess.process == null && $scope.newProcess.processingTime == null)
 			return;
 
-		mySocket.emit('client_data', $scope.newProcess);
+		var newProcess = new processClass($scope.processList.length + 1,
+										$scope.newProcess.process,
+										 $scope.newProcess.processingTime,
+										 $scope.newProcess.processingTime,
+										 readyStatus);
+		$scope.processList.push(newProcess);
+
+		$scope.newProcess.process = null;
+		$scope.newProcess.processingTime = null;
+		mySocket.emit('newProcess', newProcess);
 	};
 	
 	mySocket.on("sendProcessList", function(data){
@@ -24,4 +33,84 @@ app.controller('ProcessManagerController', ['$scope', 'mySocket', function($scop
 		$scope.newProcess.process = null;
 		$scope.newProcess.processingTime = null;
 	});
+
+	var sleep = function(milliseconds) {
+	  var start = new Date().getTime();
+	  for (var i = 0; i < 1e7; i++) {
+	    if ((new Date().getTime() - start) > milliseconds){
+	      break;
+	    }
+	  }
+	}
+
+	var readyStatus = "ready";
+	var runningStatus = "running"; 
+	var waitingStatus = "waiting"; 
+	var terminatedStatus = "terminated";
+
+	var processClass = class Process{
+		constructor(_id, _process, _processingTime, _remainingTime, _status){
+			this.id = _id;
+			this.process = _process;
+			this.processingTime = _processingTime;
+			this._remainingTime = _remainingTime;
+			this.status = _status
+		}
+
+		get remainingTime(){
+			return this._remainingTime;
+		}
+
+		set remainingTime(value){
+			this._remainingTime = value
+		}
+	}
+
+	$scope.allProcess = function(actualProcess){
+        actualProcess++;   
+        console.log(actualProcess + 1, "actualProcess");/**/ 
+        var process = $scope.processList[actualProcess];
+        if(!process){
+            return;    
+        }
+        console.log($scope.processList[actualProcess], "Process");/**/
+        process.status = runningStatus;
+        //mySocket.emit('sendProcessList', processList);
+        //mySocket.broadcast.emit('sendProcessList', processList);   
+
+        $scope.run(process);
+
+        $timeout(function(){
+        	$scope.allProcess(actualProcess);
+        }, process.processingTime * 1000);
+	};
+
+    $scope.run = function(process){
+        var remainingTime = process.remainingTime;
+        if(remainingTime == 0){
+        	process.status = terminatedStatus;
+        	mySocket.emit('sendProcessList', $scope.processList);
+            return;
+        }else{
+            $timeout(function () {
+            	process.remainingTime--;
+	            mySocket.emit('sendProcessList', $scope.processList);
+	            //mySocket.broadcast.emit('sendProcessList', processList);
+	            $scope.run(process);
+	            console.log(process.remainingTime, "id = " + process.id);/**/
+            }, 1000);
+            //sleep(1000); 
+        }
+    };
+
+    $scope.startProcessing = function(){
+    	var actualProcess = -1;
+       	$scope.allProcess(actualProcess);
+    };
+
+    $scope.$watchCollection('processList', function (newValue, oldValue){
+    	console.log('processList!!');
+    }, true);
+
+
 }]);
--- Server.js
+++ Server.js
@@ -66,36 +66,51 @@ var server = http.createServer(function(request, response) {
     }  
 });  
 server.listen(port);
-console.log("Server listenning in port: " + port);
+console.log('Server listenning in port: ' + port + '...');
 var listener = io.listen(server);  
 
 var processList = [];
+var readyStatus = "ready";
+var runningStatus = "running"; 
+var waitingStatus = "waiting"; 
+var terminatedStatus = "terminated";
+
+var sendProcessList = function(socket){
+    socket.emit('sendProcessList', processList);
+    socket.broadcast.emit('sendProcessList', processList);   
+};
 
 listener.sockets.on('connection', function(socket) {  
     //Send Data From Server To Client  
-    socket.emit('sendProcessList', processList);  
+    socket.emit('sendProcessList', processList);
+
+    socket.on('sendProcessList', function(data){
+        socket.broadcast.emit('sendProcessList', data);
+    });      
+
     //Receive Data From Client  
-    socket.on('client_data', function(data) {
+    socket.on('newProcess', function(data) {
         data.id = processList.length + 1;  
+        data.status = readyStatus;
+        data.remainingTime = data.processingTime; 
         processList.push(data);
-        socket.emit('sendProcessList', processList);  
-        socket.emit('postNewProcessSuccess', null);  
 
-        socket.broadcast.emit('sendProcessList', processList);
+        sendProcessList(socket);
+        socket.broadcast.emit('postNewProcessSuccess', null);  
+        process.stdout.write(data.process + ': ' + data.processingTime);  
+    });
 
-        process.stdout.write(data.process);  
-        console.log("");  
-        console.log(data.processingTime);  
+    socket.on('updateProcess', function(data){
+        processList.forEach(function(processs){
+            if(processs.id == data.id){
+                processs.status = data.status;
+                processs.remainingTime = data.remainingTime;
+                return;
+            }
+        });
+        socket.emit('sendProcessList', processList);
     });  
+
 });
 
-function handleRequest(request, response){
-    try {
-        //log the request on console
-        console.log(request.url);
-        //Disptach
-        dispatcher.dispatch(request, response);
-    } catch(err) {
-        console.log(err);
-    }
-} 
+ 
